639aede7217ba68f1cd990d76deec0e6c8654b3b
Juzahak <edevaldo.jr.barboza@gmail.com>
2022-11-19 15:32:26 -0300

    att

--- products/index.js
+++
@@ -1,8 +0,0 @@
-import nc from "next-connect";
-import onError from "../common/errormiddleware";
-import {
-  getAllEmployees,
-} from "../controller/products/products";
-const handler = nc(onError);
-handler.get(getAllEmployees);
-export default handler;
